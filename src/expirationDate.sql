5b1862f75e72d2d872e153469980ea8436b6bbaa
Cadenmf24 <cadenmf24@gmail.com>
2023-03-14 19:23:19 -0400

    one more thing

--- src/expirationDate.sql
+++ src/expirationDate.sql
@@ -1,7 +1,12 @@
-CREATE TABLE ExpirationDates(
+CREATE TABLE expirationDate(
     id SERIAL PRIMARY KEY NOT NULL,
     itemName TEXT NOT NULL,
     SKU INT NOT NULL,
     amount INT NOT NULL DEFAULT 1
     dateExpired TIMESTAMP NOT NULL,
 )
+
+
+INSERT INTO expirationDate(itemName, SKU, amount, dateExpired) VALUES
+    ('Test1', 1234567890, 21, '2020-01-01');
+    
